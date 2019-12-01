c0ba9b8da7d0d90ba4f14b7495274f44ce306dca
leftsweety <2015213223@bupt.edu.cn>
2019-11-30 18:28:31 -0800

    Playease 1.1.5 for Zuotian Li

--- src/main/java/edu/cmu/andrew/sweetkoala/server/http/interfaces/MainHttpInterface.java
+++ src/main/java/edu/cmu/andrew/sweetkoala/server/http/interfaces/MainHttpInterface.java
@@ -1,16 +1,26 @@
 package edu.cmu.andrew.sweetkoala.server.http.interfaces;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import edu.cmu.andrew.sweetkoala.server.exceptions.AppException;
+import edu.cmu.andrew.sweetkoala.server.http.exceptions.HttpBadRequestException;
+import edu.cmu.andrew.sweetkoala.server.http.exceptions.HttpInternalServerException;
 import edu.cmu.andrew.sweetkoala.server.http.responses.AppResponse;
-import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.annotation.security.PermitAll;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+import edu.cmu.andrew.sweetkoala.server.managers.Manager;
+import edu.cmu.andrew.sweetkoala.server.managers.WeatherManager;
+import edu.cmu.andrew.sweetkoala.server.http.interfaces.HttpInterface;
+import edu.cmu.andrew.sweetkoala.server.utils.AppLogger;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.json.JSONException;
+
+import javax.annotation.security.PermitAll;
+
 import javax.ws.rs.core.Response;
 import java.util.TreeMap;
-
 
 @Path("")
 
@@ -41,6 +51,35 @@ public class MainHttpInterface extends ResourceConfig {
         TreeMap<String,String> version = new TreeMap<>();
         version.put("Release Version", "1.0");
         return new AppResponse(version);
+    }
+
+    @POST
+    @Path("/reset")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public AppResponse resetOverall(Object request){
+
+        try{
+
+            Manager.getInstance().resetOverall();
+
+            return new AppResponse("Insert Successful");
+
+        }catch (Exception e){
+
+            throw handleException("reset error",e);
+        }
+
+    }
+
+    protected WebApplicationException handleException(String message, Exception e){
+        if(e instanceof JSONException)
+            return new HttpBadRequestException(-1, "Bad request data provided: " + e.getMessage());
+        if (e instanceof AppException)
+            return ((AppException) e).getHttpException();
+
+        AppLogger.error(message, e);
+        return new HttpInternalServerException(-1);
     }
 
 
